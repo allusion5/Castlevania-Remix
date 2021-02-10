b3da13f0a611ca0a95fe0713494bc5785e23a3f8
JorCar26 <70410997+JorCar26@users.noreply.github.com>
2021-02-10 15:01:09 -0500

    MergerWorked?

---
+++ Assets/BossController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossController : MonoBehaviour
+{
+    public CapsuleCollider2D bossHead;
+    public Transform bossTransform;
+    public int bossHealth;
+    public int facing;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
